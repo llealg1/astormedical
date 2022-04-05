b912baa9f58a2a8e05b8760f25f9a261c226828c
lleag1 <luislealgalicia@gmail.com>
2022-04-05 06:49:23 -0400

    add carousel

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
+import Carousel from "../components/Carousel";
 import BasicLayout from "../layouts/BasicLayout";
 
 export default function Home() {
   return (
     <BasicLayout>
+      <Carousel />
     </BasicLayout>
   )
 }
