fb933e58a525f5b5a3d5f427ddd6c198fb79cd6b
robert-dev <kaleft212@gmail.com>
2022-05-17 14:12:14 -0400

    done

--- components/Header/TopBar/TopBar.jsx
+++ components/Header/TopBar/TopBar.jsx
@@ -1,32 +1,30 @@
-import {Container, Grid, Image, Input} from 'semantic-ui-react'
-import Link from 'next/link'
-import Nav from '../Nav'
+import { Container, Grid, Image, Input } from "semantic-ui-react";
+import Link from "next/link";
+import Nav from "../Nav";
 
 export default function TopBar() {
   return (
-    <div className='top-bar'>
-      <Container fluid >
-        <Grid className='top-bar'>
+    <div className="top-bar">
+      <Container fluid>
+        <Grid className="top-bar">
           <Grid.Column width={8} className="top-bar__left">
             <Logo />
           </Grid.Column>
           <Grid.Column width={8} className="top-bar__right">
-            <Nav/>
+            <Nav />
           </Grid.Column>
         </Grid>
       </Container>
     </div>
-  )
-
+  );
 }
 
 function Logo() {
   return (
-    <Link href='/'>
-      <a >
-        <Image src='/astormedic.png' alt='Astor Medical'size='tiny'/>
+    <Link href="/">
+      <a>
+        <Image src="/astormedic.png" alt="Astor Medical" size="tiny" />
       </a>
     </Link>
-  )
+  );
 }
-
