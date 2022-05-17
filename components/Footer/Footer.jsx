0145fac3371fbc5e5a67a138e4c19e9ca1ba8a82
robert-dev <kaleft212@gmail.com>
2022-05-17 14:05:08 -0400

    footer listo

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,69 +1,193 @@
-import { Container, Row, Col, Nav } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Image } from "semantic-ui-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMap, faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {
-  faFacebookSquare,
+  faMapLocationDot,
+  faPhone,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
   faInstagram,
-  faTwitterSquare,
+  faWhatsapp,
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
-    <Container fluid className="footer text-center">
-      <Row className="container-row">
-        <Col className="about-us">
-          <h3>AstorMedical C.A</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam,
-            officiis! Et odio ipsam non eum nihil corrupti architecto,
-            voluptatum, molestias officia iure fugiat sequi facere, velit
-            aspernatur! Temporibus, vel error?
-          </p>
-        </Col>
-        <Col className="contact mx-auto">
-          <h3>Contacto</h3>
-          <p className="d-flex align-items-center">
-            <FontAwesomeIcon icon={faMap} />
-            <a href="#">Ubicación</a>
-          </p>
-          <p className="d-flex align-items-center">
-            <FontAwesomeIcon icon={faPhone} />
-            <a href="#">0412-000000</a>
-          </p>
-          <p className="d-flex align-items-center">
-            <FontAwesomeIcon icon={faPhone} />
-            <a href="#">0412-000000</a>
-          </p>
-          <p className="d-flex align-items-center">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <a href="#">Correo: mail@mail.com</a>
-          </p>
-        </Col>
-        <Col className="menu">
-          <h3>Menú</h3>
-          <Nav className="flex-column">
-            <Nav.Link>Inicio</Nav.Link>
-            <Nav.Link>Nosotros</Nav.Link>
-            <Nav.Link>Productos</Nav.Link>
-            <Nav.Link>Blog</Nav.Link>
-          </Nav>
-        </Col>
-        <Col className="social-networks">
-          <h3>Nuestras Redes Sociales</h3>
-          <p className="d-flex justify-content-center align-items-center">
-            <FontAwesomeIcon icon={faFacebookSquare} />
-            <a href="#"> AstorMedical </a>
-          </p>
-          <p className="d-flex justify-content-center align-items-center">
-            <FontAwesomeIcon icon={faInstagram} />
-            <a href="#"> AstorMedical </a>
-          </p>
-          <p className="d-flex justify-content-center align-items-center">
-            <FontAwesomeIcon icon={faTwitterSquare} />
-            <a href="#"> AstorMedical </a>
-          </p>
-        </Col>
-      </Row>
+    <Container fluid className="mt-5 p-0">
+      <footer className=" text-center text-lg-start bg-light">
+        {/* Social media */}
+        <section className="d-flex social-network justify-content-center align-items-center justify-content-lg-between p-4">
+          {/* left */}
+          <div className="d-none fw-bold d-lg-block">
+            <span className="ms-5 fs-5">
+              Contacta con nosotros a través de nuestras redes sociales:
+            </span>
+          </div>
+          {/* left */}
+
+          {/* right */}
+          <div className="me-5 social-icons">
+            <a
+              class="btn btn-primary border-0 shadow-sm btn-floating fs-5 m-1"
+              href="#!"
+              style={{ backgroundColor: "#3b5998" }}
+              role="button"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a
+              class="btn btn-primary border-0 shadow-sm btn-floating fs-5 m-1"
+              href="#!"
+              style={{ backgroundColor: "#C13584" }}
+              role="button"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a
+              class="btn btn-primary border-0 shadow-sm btn-floating fs-5 m-1"
+              href="https://api.whatsapp.com/send/?phone=584126036107&text&app_absent=0"
+              target={"_blank"}
+              style={{ backgroundColor: "#25D366" }}
+              role="button"
+            >
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </a>
+          </div>
+          {/* right */}
+        </section>
+        {/* Social media */}
+
+        {/* about */}
+        <section className="footer pt-2">
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-5">
+                <a className="mb-4 logo">
+                  <Image src="/astormedic.png" className="mb-3" size="tiny" />
+                </a>
+                <span className="about-us">
+                  Somos una empresa que provee equipamiento médico, de insumos y
+                  accesorios con excelentes estándares de calidad. Contamos con
+                  laboratorio técnico para mantenimiento correctivo, preventivo,
+                  de esterilización y desinfección de equipos.
+                </span>
+              </div>
+              {/* about */}
+
+              {/* links */}
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-5">
+                <h3 className="links text-uppercase fw-bold mb-4">Menú</h3>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Inicio
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Nosotros
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Productos
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Blog
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Contacto
+                  </a>
+                </p>
+              </div>
+              {/* links */}
+
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-5">
+                <h3 className="catalog text-uppercase fw-bold mb-4">
+                  Cátalogo
+                </h3>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Insumos
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Equipos Médicos
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Laboratorios Médicos
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-reset">
+                    Instrumentos y Accesorios
+                  </a>
+                </p>
+              </div>
+              {/* links */}
+
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-5">
+                <h3 className="text-uppercase fw-bold mb-4">Contactanos</h3>
+                <p>
+                  <a
+                    href="https://www.google.com/maps/place/astor+medical/@10.4969911,-66.8575217,17.83z/data=!4m19!1m13!4m12!1m4!2m2!1d-67.9061688!2d10.2032677!4e1!1m6!1m2!1s0x87176f5159b1dc67:0xa118448b9f2b3660!2sastor+medical!2m2!1d-66.8565051!2d10.4977242!3m4!1s0x87176f5159b1dc67:0xa118448b9f2b3660!8m2!3d10.4977242!4d-66.8565051"
+                    target={"_blank"}
+                    className="text-reset"
+                  >
+                    <FontAwesomeIcon
+                      icon={faMapLocationDot}
+                      className="me-2"
+                      style={{ color: "#fff" }}
+                    />
+                    Centro San Ignacio, Edificio Torre Copérnico, Piso 4,
+                    Oficina 406. Av. Blandín, Miranda 1060, Estado Miranda.
+                  </a>
+                </p>
+                <p>
+                  <a href="mailto:Info@astormedic.com" className="text-reset">
+                    <FontAwesomeIcon
+                      icon={faEnvelope}
+                      className="me-2"
+                      style={{ color: "#fff" }}
+                    />
+                    info@astormedic.com
+                  </a>
+                </p>
+                <p>
+                  <a
+                    href="https://api.whatsapp.com/send/?phone=584126036107&text&app_absent=0"
+                    target={"_blank"}
+                    className="text-reset"
+                  >
+                    <FontAwesomeIcon
+                      icon={faPhone}
+                      className="me-2"
+                      style={{ color: "#fff" }}
+                    />
+                    (+58) 412-6036107
+                  </a>
+                </p>
+              </div>
+              {/* links */}
+            </div>
+          </div>
+          {/* copyright */}
+          <div className="text-center text-white p-4">
+            © 2022 Copyright:
+            <a class="text-reset ms-1 fw-bold" href="#">
+              Astormedical.com
+            </a>
+          </div>
+          {/* copyright */}
+        </section>
+      </footer>
     </Container>
   );
 }
